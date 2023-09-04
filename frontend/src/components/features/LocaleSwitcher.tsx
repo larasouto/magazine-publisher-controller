a51243da1411d324a2611aeb26a0141dea714aa2
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2023-09-04 04:04:39 -0300

    refactor: change switches

--- frontend/src/components/features/LocaleSwitcher.tsx
+++ frontend/src/components/features/LocaleSwitcher.tsx
@@ -1,4 +1,5 @@
 import { Lang } from '@/i18n'
+import { cn } from '@/lib/utils'
 import {
   Avatar,
   Button,
@@ -7,7 +8,7 @@ import {
   DropdownMenu,
   DropdownTrigger
 } from '@nextui-org/react'
-import { Languages } from 'lucide-react'
+import { Languages, Loader2 } from 'lucide-react'
 import { useTranslation } from 'react-i18next'
 
 type Language = {
@@ -32,24 +33,41 @@ const langs: Language[] = [
 export const LocaleSwitcher = () => {
   const { i18n } = useTranslation()
 
+  const resetZodErrors = () => {
+    document
+      .querySelectorAll('[id^="react-aria"].text-tiny.text-danger')
+      .forEach((element) => (element.innerHTML = ''))
+  }
+
   const setLanguage = (lang: Lang) => {
+    if (lang !== i18n.language) {
+      resetZodErrors()
+    }
+
     i18n.changeLanguage(lang)
   }
 
   return (
-    <div className="flex gap-2">
+    <div className="flex gap-2 z-50">
       <Dropdown placement="bottom-end">
         <DropdownTrigger>
           <Button isIconOnly variant="flat">
             <Languages size={20} />
           </Button>
         </DropdownTrigger>
-        <DropdownMenu variant="faded" aria-label="Dropdown menu with icons">
+        <DropdownMenu variant="faded">
           {langs.map((lang) => (
             <DropdownItem
               key={lang.code}
-              startContent={<Avatar className="w-5 h-5" src={lang.link} />}
+              startContent={
+                <Avatar
+                  fallback={<Loader2 className="animate-spin" />}
+                  className="w-5 h-5"
+                  src={lang.link}
+                />
+              }
               onClick={() => setLanguage(lang.code)}
+              className={cn({ 'text-primary': lang.code === i18n.language })}
             >
               {lang.label}
             </DropdownItem>
--- frontend/src/components/features/ThemeSwitcher.tsx
+++ frontend/src/components/features/ThemeSwitcher.tsx
@@ -1,16 +1,25 @@
-import { useTheme } from '@/hooks/useTheme'
+import { useThemeStore } from '@/stores/useThemeStore'
 import { Button } from '@nextui-org/react'
 import { Moon, Sun } from 'lucide-react'
+import { useEffect } from 'react'
 
 export const ThemeSwitcher = () => {
-  const { theme, setTheme } = useTheme()
+  const [theme, setTheme] = useThemeStore((state) => [
+    state.theme,
+    state.setTheme
+  ])
 
   const toggleTheme = () => {
     setTheme(theme === 'dark' ? 'light' : 'dark')
   }
 
+  useEffect(() => {
+    window.document.documentElement.removeAttribute('class')
+    window.document.documentElement.classList.add(theme)
+  }, [theme])
+
   return (
-    <Button onClick={toggleTheme} variant="flat" isIconOnly>
+    <Button onClick={toggleTheme} variant="flat" className="z-50" isIconOnly>
       {theme === 'dark' ? <Sun /> : <Moon />}
     </Button>
   )
