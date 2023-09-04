5cb2346fbc6c2fef72545659904c974a13b2753b
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2023-09-04 19:17:09 -0300

    chore: update dependencies

--- backend/src/infra/http/server.ts
+++ backend/src/infra/http/server.ts
@@ -1,7 +1,8 @@
+import { green } from 'console-log-colors'
 import { server } from './app'
 
 const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => {
-  console.log(`\n🚀 Server started at http://localhost:${PORT}`)
+  console.log('\n🚀 Server started at', green(`http://localhost:${PORT}`))
 })
