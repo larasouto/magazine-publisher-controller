d0d404fabe7ccfe577e9f03acce4c33c140251c5
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2023-09-04 04:07:27 -0300

    refactor: change props entity

--- backend/src/core/domain/entity.ts
+++ backend/src/core/domain/entity.ts
@@ -1,7 +1,7 @@
 import { v4 as uuid } from 'uuid'
 
-export class Entity<T> {
-  public readonly _id: string
+export abstract class Entity<T> {
+  protected readonly _id: string
   public readonly props: T
 
   constructor(props: T, id?: string) {
