8e34a895e7bb04a7d7f04da0decddb34b9b02e96
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2023-09-13 02:58:00 -0300

    unrelated: this is just a test

--- frontend/src/routes/pages/home.route.tsx
+++ frontend/src/routes/pages/home.route.tsx
@@ -1,5 +1,5 @@
 import { ComponentLayout } from '@/layout/ComponentLayout'
-import JustForTests from '@/pages/JustForTests'
+import { JustForTestsDoNotUseIt } from '@/pages/home/JustForTestsDoNotUseIt'
 import { AuthGuard } from '@/utils/guard/AuthGuard'
 import { routes } from '@routes'
 import { RouteObject } from 'react-router-dom'
@@ -17,7 +17,7 @@ export const HomeRoutes: RouteObject = {
       children: [
         {
           path: routes.home.index,
-          element: <JustForTests />
+          element: <JustForTestsDoNotUseIt />
         }
       ]
     }
