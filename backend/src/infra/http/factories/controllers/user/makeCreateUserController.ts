85d4548d704c38e9ffc7f0ff459dc6af523c48a2
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2023-09-13 02:56:22 -0300

    style: rename factory controllers

--- backend/src/infra/http/factories/controllers/user/makeCreateUserController.ts
+++ backend/src/infra/http/factories/controllers/user/makeCreateUserController.ts
@@ -4,17 +4,18 @@ import { CreateUserController } from '@/application/users/use-cases/create-user/
 import { ValidatorCompositor } from '@/infra/validation/ValidatorCompositor'
 import { CompareFieldsValidator } from '@/infra/validation/CompareFieldsValidator'
 import { t } from 'i18next'
+import { Controller } from '@/core/infra/controller'
 
-export function makeCreateUserController() {
+export function makeCreateUserController(): Controller {
   const prismaUsersRepository = new PrismaUsersRepository()
   const createUser = new CreateUser(prismaUsersRepository)
 
   const validator = new ValidatorCompositor([
-    new CompareFieldsValidator(
-      'password',
-      'confirmPassword',
-      t('errors.passwords_dont_match'),
-    ),
+    new CompareFieldsValidator({
+      field: 'password',
+      fieldToCompare: 'confirmPassword',
+      keyMessage: 'account.passwords_dont_match',
+    }),
   ])
 
   return new CreateUserController(validator, createUser)
