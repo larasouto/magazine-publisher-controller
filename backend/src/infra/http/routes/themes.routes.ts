a7db32bd59fa18e20224ca685ea6f5644d761146
lara <larasoutom@gmail.com>
2023-09-30 20:52:36 -0300

    feat: add edit theme

--- backend/src/infra/http/routes/themes.routes.ts
+++ backend/src/infra/http/routes/themes.routes.ts
@@ -3,9 +3,11 @@ import { adaptRoute } from '@/core/infra/adapters/express-route-adapter'
 import { Router } from 'express'
 import { makeEnsureAuthenticated } from '../factories/controllers/auth/makeEnsureAuthenticated'
 import { makeCreateThemesController } from '../factories/controllers/themes/makeCreateThemeController'
+import { makeEditThemesController } from '../factories/controllers/themes/makeEditThemeController'
 
 export const themes = Router()
 
 themes.use(adaptMiddleware(makeEnsureAuthenticated()))
 
 themes.post('/new', adaptRoute(makeCreateThemesController()))
+themes.put('/:themeId/edit', adaptRoute(makeEditThemesController()))
