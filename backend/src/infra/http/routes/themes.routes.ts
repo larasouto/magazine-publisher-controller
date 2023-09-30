476b73a77e278a68c9df2f1a2d16471e3aef58e6
lara <larasoutom@gmail.com>
2023-09-30 20:55:36 -0300

    feat: add get theme

--- backend/src/infra/http/routes/themes.routes.ts
+++ backend/src/infra/http/routes/themes.routes.ts
@@ -4,6 +4,7 @@ import { Router } from 'express'
 import { makeEnsureAuthenticated } from '../factories/controllers/auth/makeEnsureAuthenticated'
 import { makeCreateThemesController } from '../factories/controllers/themes/makeCreateThemeController'
 import { makeEditThemesController } from '../factories/controllers/themes/makeEditThemeController'
+import { makeGetThemeController } from '../factories/controllers/themes/makeGetThemeController'
 
 export const themes = Router()
 
@@ -11,3 +12,4 @@ themes.use(adaptMiddleware(makeEnsureAuthenticated()))
 
 themes.post('/new', adaptRoute(makeCreateThemesController()))
 themes.put('/:themeId/edit', adaptRoute(makeEditThemesController()))
+themes.get('/:themeId', adaptRoute(makeGetThemeController()))
