8491922fe392dc78af52703febcf86a653a2d353
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2023-10-01 06:09:08 -0300

    fix: list reporters

--- backend/src/application/reporters/use-cases/list-reporter/list-reporter.controller.ts
+++ backend/src/application/reporters/use-cases/list-reporter/list-reporter.controller.ts
@@ -8,6 +8,6 @@ export class ListReportersController implements Controller {
   async handle(): Promise<HttpResponse> {
     const result = await this.listReporters.execute()
 
-    return ok({ reporters: result })
+    return ok({ dto: result })
   }
 }
