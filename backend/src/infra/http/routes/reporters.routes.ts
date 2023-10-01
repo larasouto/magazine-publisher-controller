cfe0774aaab423900be43fc49f079d4eca9a3d4c
lara <larasoutom@gmail.com>
2023-09-30 22:36:16 -0300

    feat: add edit reporter

--- backend/src/infra/http/routes/reporters.routes.ts
+++ backend/src/infra/http/routes/reporters.routes.ts
@@ -3,9 +3,11 @@ import { adaptRoute } from '@/core/infra/adapters/express-route-adapter'
 import { Router } from 'express'
 import { makeEnsureAuthenticated } from '../factories/controllers/auth/makeEnsureAuthenticated'
 import { makeCreateReportersController } from '../factories/controllers/reporters/makeCreateReporterController'
+import { makeEditReportersController } from '../factories/controllers/reporters/makeEditReporterController'
 
 export const reporters = Router()
 
 reporters.use(adaptMiddleware(makeEnsureAuthenticated()))
 
 reporters.post('/new', adaptRoute(makeCreateReportersController()))
+reporters.put('/:reporterId/edit', adaptRoute(makeEditReportersController()))
