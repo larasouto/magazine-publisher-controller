54403388bc517ebec4a1610a69db20dab1e4602f
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2023-10-17 15:49:46 -0300

    feat: add usePageUtils hook

---
+++ frontend/src/hooks/usePageTranslation.tsx
@@ -0,0 +1,33 @@
+import { BreadcrumbItem } from '@/components/ui/Breadcrumb'
+import { routes } from '@/routes/routes'
+import { useTranslation } from 'react-i18next'
+import { useParams } from 'react-router-dom'
+
+type PageProps = {
+  _default?: boolean
+  segments?: BreadcrumbItem[]
+  dynamic?: boolean
+}
+
+export const usePageUtils = (ns?: string | string[]) => {
+  const { id } = useParams()
+  const { t } = useTranslation(ns)
+
+  const title = ({ dynamic = false }: PageProps = {}) => {
+    if (dynamic) {
+      return id ? t('page.edit') : t('page.new')
+    }
+    return t('page.title')
+  }
+
+  const breadcrumb = ({ _default = true, segments }: PageProps = {}) => {
+    const content =
+      _default && !segments
+        ? [{ label: 'Home', link: routes.home.index }, { label: title() }]
+        : [{ label: 'Home', link: routes.home.index }]
+
+    return !segments ? content : [...content, ...segments]
+  }
+
+  return { id, t, title, breadcrumb }
+}
