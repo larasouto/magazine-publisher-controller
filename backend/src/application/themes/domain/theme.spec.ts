d3b1336e6de55075d178ba5a8c131c600e00abb4
larasouto <larasoutom@gmail.com>
2023-10-20 22:12:27 -0300

    fix: minor changes

--- backend/src/application/themes/domain/theme.spec.ts
+++ backend/src/application/themes/domain/theme.spec.ts
@@ -4,8 +4,17 @@ import { Theme } from './theme'
 describe('Entity Theme', () => {
   test('should be able to create a theme', () => {
     const data: any = {
-      name: 'Theme name',
-      description: 'Theme description',
+      name: 'theme-name',
+      description: 'theme-description',
+    }
+
+    const sut = Theme.create(data)
+    expect(sut.isRight()).toBeTruthy()
+  })
+
+  test('should be able to create a theme with only a name', () => {
+    const data: any = {
+      name: 'theme-name',
     }
 
     const sut = Theme.create(data)
@@ -20,14 +29,5 @@ describe('Entity Theme', () => {
 
     const sut = Theme.create(data)
     expect(sut.isLeft()).toBeTruthy()
-  })
-
-  test('should be able to create a theme with only a name', () => {
-    const data: any = {
-      name: 'Theme name',
-    }
-
-    const sut = Theme.create(data)
-    expect(sut.isRight()).toBeTruthy()
   })
 })
