7009bc41fc7647e6a52eb33499fdae81c3d0a485
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2023-11-05 21:03:46 -0300

    fix: updateParams in useFetcher hook

--- frontend/src/utils/replace-params.ts
+++ frontend/src/utils/replace-params.ts
@@ -13,3 +13,27 @@ export const replaceParams = (route: string, values: string | string[]) => {
   }
   return route?.replace(/:[^/]+/g, () => values[index++])
 }
+
+/**
+ * Substitui os valores entre `:` e `/` de uma rota dinâmica.
+ *
+ * @example
+ *  replaceParams('/test/:id', '123') -> '/test/123'
+ *  replaceParams('/test/:id/test/:testId', ['123', '345']) -> '/test/123/testId/345'
+ */
+export const updateParams = (
+  route: string,
+  values?: (string | undefined)[]
+) => {
+  let index = 0
+
+  if (!values) {
+    return route
+  }
+
+  if (values.some((v) => !v)) {
+    return route
+  }
+
+  return route?.replace(/:[^/]+/g, () => values[index++] ?? '')
+}
