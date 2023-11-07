a8db54dfd7a2d3bb020f5c75b7af074dd02eb27b
GianEsteves <115878514+GianEsteves@users.noreply.github.com>
2023-11-07 18:35:35 -0300

    feat( backend) refactoring the bookstore

--- backend/src/application/bookstore/domain/bookstore.spec.ts
+++ backend/src/application/bookstore/domain/bookstore.spec.ts
@@ -1,17 +1,17 @@
 import { describe, expect, test } from 'vitest'
 import { Bookstore } from './bookstore'
 
-describe('Entity Theme', () => {
+describe('Entity bookstore', () => {
   test('should be able to create a theme', () => {
     const data: any = {
-      address: 'Theme address',
+      address: ' address',
     }
 
     const sut = Bookstore.create(data)
     expect(sut.isRight()).toBeTruthy()
   })
 
-  test('should not be able to create a theme with invalid data', () => {
+  test('should not be able to create a bookstore with invalid data', () => {
     const data: any = {
       address: null,
     }
