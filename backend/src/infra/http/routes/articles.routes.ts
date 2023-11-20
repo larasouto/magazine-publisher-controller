d871340d67eb588cf550037aaa75355290d4a70f
larasouto <larasoutom@gmail.com>
2023-11-19 22:40:33 -0300

    feat: add edit article

--- backend/src/infra/http/routes/articles.routes.ts
+++ backend/src/infra/http/routes/articles.routes.ts
@@ -3,8 +3,10 @@ import { adaptRoute } from '@/core/infra/adapters/express-route-adapter'
 import { Router } from 'express'
 import { makeEnsureAuthenticated } from '../factories/controllers/auth/makeEnsureAuthenticated'
 import { makeCreateArticlesController } from '../factories/controllers/articles/makeCreateArticlesController'
+import { makeEditArticlesController } from '../factories/controllers/articles/makeEditArticlesController'
 export const articles = Router()
 
 articles.use(adaptMiddleware(makeEnsureAuthenticated()))
 
 articles.post('/new', adaptRoute(makeCreateArticlesController()))
+articles.post('/:articleId/edit', adaptRoute(makeEditArticlesController()))
