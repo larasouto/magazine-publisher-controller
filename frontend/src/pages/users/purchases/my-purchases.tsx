cacd1c670f33c56cf0a76dbe8fe745fa57d9e4a1
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2023-11-21 16:02:23 -0300

    feat: add shape in my-purchases

--- frontend/src/pages/users/purchases/my-purchases.tsx
+++ frontend/src/pages/users/purchases/my-purchases.tsx
@@ -1,7 +1,9 @@
+import { Format } from '@/components/ui/label/Format'
 import { useFetch } from '@/hooks/useFetch'
 import { usePageUtils } from '@/hooks/usePageTranslation'
 import { PageLayout } from '@/layout/PageLayout'
 import { backend } from '@/routes/routes'
+import { Button, Image } from '@nextui-org/react'
 
 export const MyPurchasesPage = () => {
   const { breadcrumb } = usePageUtils()
@@ -27,9 +29,37 @@ export const MyPurchasesPage = () => {
       })}
     >
       <section className="flex flex-col gap-3">
-        <div className="flex flex-col gap-2">
-          <div className="bg-default-50 rounded-lg">
-            <h1>Teste</h1>
+        <div className="flex flex-col gap-3">
+          <div className="bg-default-50 rounded-lg p-5">
+            <div className="flex gap-3">
+              <Image src="/revista1.jpg" width={100} height={100} />
+              <div className="flex flex-col gap-1.5 max-w-lg">
+                <div className="flex flex-col">
+                  <label className="tracking-wide text-tiny text-default-500">
+                    Título
+                  </label>
+                  <h1 className="font-bold line-clamp-1">Revista 1</h1>
+                </div>
+                <div className="flex flex-col">
+                  <label className="tracking-wide text-tiny text-default-500">
+                    Descrição
+                  </label>
+                  <p className="line-clamp-2">Revista 2</p>
+                </div>
+                <div className="flex flex-col">
+                  <label className="tracking-wide text-tiny text-default-500">
+                    Valor pago
+                  </label>
+                  <p className="line-clamp-1">
+                    <Format text={'49.90'} size="sm" type="price" />
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="flex gap-3 mt-5">
+              <Button color="primary">Comprar novamente</Button>
+              <Button>Comentar</Button>
+            </div>
           </div>
         </div>
       </section>
