3c7e7bfae77998cd5e87104a1183f25e15f8a065
larasouto <larasoutom@gmail.com>
2023-12-18 05:50:14 -0300

    feat: add edit offer

--- backend/src/application/offers/repositories/interfaces/IOffersRepository.ts
+++ backend/src/application/offers/repositories/interfaces/IOffersRepository.ts
@@ -1,10 +1,9 @@
-import { Edition } from '@/application/editions/domain/edition'
 import { Offer } from '@/application/offers/domain/offer'
 
 export interface IOffersRepository {
   findById(id: string): Promise<Offer | null>
   create(offer: Offer, editions: string[]): Promise<void>
-  update(offer: Offer): Promise<void>
+  update(offer: Offer, editions: string[]): Promise<void>
   deleteMany(ids: string[]): Promise<void>
   list(): Promise<Offer[]>
   exists(id: string): Promise<boolean>
--- backend/src/infra/http/routes/offers.routes.ts
+++ backend/src/infra/http/routes/offers.routes.ts
@@ -3,9 +3,11 @@ import { adaptRoute } from '@/core/infra/adapters/express-route-adapter'
 import { Router } from 'express'
 import { makeEnsureAuthenticated } from '../factories/controllers/auth/makeEnsureAuthenticated'
 import { makeCreateOffersController } from '../factories/controllers/offers/makeCreateOffersController'
+import { makeEditOffersController } from '../factories/controllers/offers/makeEditOffersController'
 
 export const offers = Router()
 
 offers.use(adaptMiddleware(makeEnsureAuthenticated()))
 
 offers.post('/new', adaptRoute(makeCreateOffersController()))
+offers.put('/:offerId/edit', adaptRoute(makeEditOffersController()))
